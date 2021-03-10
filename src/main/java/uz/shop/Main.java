c9b702db2cd8f7455f28e536f18dff0c682d80c9
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-03-10 12:35:15 +0500

    Working state. And change connection to PostgreSql

--- src/main/java/uz/shop/Main.java
+++ src/main/java/uz/shop/Main.java
@@ -6,8 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import uz.shop.config.AppConfig;
+import uz.shop.config.PersistenceConfig;
 
 public class Main extends Application {
+
+    public static AnnotationConfigApplicationContext ctx;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,6 +20,10 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        ctx = new AnnotationConfigApplicationContext();
+        ctx.register(PersistenceConfig.class);
+       // ctx.register(AppConfig.class);
+        ctx.refresh();
     }
 
 
